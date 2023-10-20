7b6e785cdb9a621f2c3c31cb6fc5d5569e675cf7
Nate-go <140036945+Nate-go@users.noreply.github.com>
2023-10-20 12:45:49 +0700

    CH-5 setup for project (#1)

---
+++ src/assets/images/index.js
@@ -0,0 +1,2 @@
+export { default as logo_image} from './logo-web.png'
+export { default as vertical_bg } from './vertical-bg.jpg'
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import App from '@/App.jsx'
+import '@/index.css'
+import { BrowserRouter } from 'react-router-dom';
+import 'mdb-react-ui-kit/dist/css/mdb.min.css';
+import "@fortawesome/fontawesome-free/css/all.min.css";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  <BrowserRouter>
     <App />
-  </React.StrictMode>,
+  </BrowserRouter>,
 )
