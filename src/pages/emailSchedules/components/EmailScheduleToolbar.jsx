e69650e3b3424430e9a11bfe046a13f64a680ab0
Nate <nate.ha.goldenowl@gmail.com>
2023-12-07 08:41:18 +0700

    CH-37 Udate

--- src/pages/emailSchedules/components/EmailScheduleToolbar.jsx
+++ src/pages/emailSchedules/components/EmailScheduleToolbar.jsx
@@ -8,11 +8,11 @@ import { AccessStatus } from '@/constants';
 import { AutoLoader } from '@/components';
 import { StatusSingleSelect } from '@/components/selects';
 
-const EmailCheduleToolbar = ({checkedKeys, deleteTemplateGroups, changeStatus, openConfirmation, setFetch}) => {
+const EmailCheduleToolbar = ({ checkedKeys, deleteEmailSchedules, changeStatus, openConfirmation, setFetch}) => {
     return (
         <>
             <ButtonToolbar className='pb-4'>
-                <Button disabled={!checkedKeys.length} color="red" className='bg-red-600' appearance="primary" startIcon={<TrashIcon />} onClick={deleteTemplateGroups}>
+                <Button disabled={!checkedKeys.length} color="red" className='bg-red-600' appearance="primary" startIcon={<TrashIcon />} onClick={deleteEmailSchedules}>
                     Delete
                 </Button>
                 <Button disabled={!checkedKeys.length} color="cyan" className='bg-cyan-500' appearance="primary" startIcon={<TbStatusChange />} onClick={changeStatus}>
